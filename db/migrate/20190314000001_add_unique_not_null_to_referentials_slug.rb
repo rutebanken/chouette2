a4ae6e7798215bd8257963e826dd8a22094eecc2
vincent <vincent.paturet@entur.org>
2019-03-25 09:13:33 +0100

    ROR-612 Provider creation from admin interface

--- db/migrate/20190314000001_add_unique_not_null_to_referentials_slug.rb
+++
@@ -1,8 +0,0 @@
-class AddUniqueNotNullToReferentialsSlug < ActiveRecord::Migration
-
-  def up
-    change_column_null :referentials, :slug, false
-    add_index(:referentials, [:slug], unique: true) unless index_exists?(:referentials, [:slug], unique: true)
-  end
-
-end
