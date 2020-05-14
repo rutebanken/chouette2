6e7b22e06172f1843f9a1498fefb07d905e7114b
vincent <vincent.paturet@entur.org>
2020-05-14 09:13:57 +0200

    ROR-813- Add support for multilingual notices

---
+++ db/migrate/20200511163000_add_footnote_alternative_texts.rb
@@ -0,0 +1,17 @@
+class AddFootnoteAlternativeTexts < ActiveRecord::Migration
+  def change
+    create_table :footnote_alternative_texts, :id => false, :force => true do |t|
+      t.string   "objectid",                  null: false
+      t.integer  "object_version"
+      t.datetime "creation_time"
+      t.string   "creator_id",                limit: 255
+      t.integer  "footnote_id", :null => false
+      t.string   "text", :null => false
+      t.string   "language", :null => false
+    end
+    add_index "footnote_alternative_texts", ["objectid"], :name => "footnote_alternative_texts_objectid_key", :unique => true
+    add_index :footnote_alternative_texts, [:footnote_id, :language], :name => "footnote_alternative_texts_footnote_id_language", :unique => true
+    add_foreign_key "footnote_alternative_texts", "footnotes", name: "footnotes_footnote_alternative_texts_fkey"
+
+  end
+end
