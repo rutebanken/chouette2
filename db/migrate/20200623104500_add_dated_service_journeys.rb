aae85549cbf340bab90c65d9e3438fd5b2eb7a77
vincent <vincent.paturet@entur.org>
2020-07-07 09:27:40 +0200

    ROR-872 Implement DatedServiceJourney

--- db/migrate/20200623104500_add_dated_service_journeys.rb
+++ db/migrate/20200623104500_add_dated_service_journeys.rb
@@ -7,12 +7,20 @@ class AddDatedServiceJourneys < ActiveRecord::Migration
       t.string   "creator_id",                limit: 255
       t.date  "operating_day", :null => false
       t.integer  "vehicle_journey_id", :null => false
-      t.integer  "derived_from_id"
       t.string   "service_alteration"
     end
     add_index "dated_service_journeys", ["objectid"], :name => "dated_service_journeys_objectid_key", :unique => true
     add_foreign_key "dated_service_journeys", "vehicle_journeys", name: "vehicle_journeys_fkey"
     add_foreign_key "dated_service_journeys", "dated_service_journeys", column: "derived_from_id", name: "derived_from_fkey"
+
+    create_table :original_dated_service_journeys, :id => false, :force => true do |t|
+      t.integer  "original_dated_service_journey_id"
+      t.integer  "derived_dated_service_journey_id"
+    end
+    add_index "original_dated_service_journeys", ["original_dated_service_journey_id", "derived_dated_service_journey_id"], :unique => true, :name => "original_dated_service_journeys_idx"
+    add_index "original_dated_service_journeys", ["derived_dated_service_journey_id"], :name => "derived_dated_service_journeys_idx"
+    add_foreign_key "original_dated_service_journeys", "dated_service_journeys", column: "original_dated_service_journeys_id", name: "original_dated_service_journeys_id_fkey"
+    add_foreign_key "original_dated_service_journeys", "dated_service_journeys", column: "derived_dated_service_journeys_id", name: "derived_dated_service_journeys_id_fkey"
 
   end
 end
