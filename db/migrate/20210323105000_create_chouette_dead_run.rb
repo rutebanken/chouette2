38be5e5a659ab1324da5554b93b29605622f23f2
vincent <vincent.paturet@entur.org>
2021-03-25 15:57:54 +0100

    Add support for DeadRuns

--- db/migrate/20210323105000_create_chouette_dead_run.rb
+++ db/migrate/20210323105000_create_chouette_dead_run.rb
@@ -1,6 +1,7 @@
 class CreateChouetteDeadRun < ActiveRecord::Migration
   def up
 
+    connection.execute 'drop table time_tables_dead_runs'
     connection.execute 'drop table if exists dead_run_at_stops'
     connection.execute 'drop table if exists blocks_dead_runs'
     connection.execute 'drop table if exists dead_runs'
@@ -17,10 +18,18 @@ class CreateChouetteDeadRun < ActiveRecord::Migration
    add_foreign_key "dead_runs", "journey_patterns", column: "journey_pattern_id",  name: "dead_runs_journey_patterns_id_fkey"
 
     create_table :dead_run_at_stops, :force => true do |t|
+
+      t.string   "objectid", :null => false
+      t.integer  "object_version"
+      t.datetime "creation_time"
+      t.string   "creator_id"
+
       t.integer  "dead_run_id", :limit => 8
       t.integer  "stop_point_id", :limit => 8
       t.time "arrival_time"
       t.time "departure_time"
+      t.integer "arrival_day_offset"
+      t.integer "departure_day_offset"
     end
     add_index "dead_run_at_stops", ["dead_run_id"], :name => "index_dead_run_at_stops_on_dead_run_id"
     add_index "dead_run_at_stops", ["stop_point_id"], :name => "index_dead_run_at_stops_on_stop_pointid"
